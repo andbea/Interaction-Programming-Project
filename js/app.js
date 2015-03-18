4adae1e5358ca7016b5e64c0cdff1497f8dbac1e
andbea <andbea@kth.se>
2015-03-18 11:11:32 +0100

    Revert "adding day view in app"
    This reverts commit da5a5da6583f6e1bd6b1b1f94fc375faa352b7a1.

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(function() {
 	// this is the instance of our main model
 	// this is what you should use in your application
 	var model = new Model();
-
+	
 	var activityView = new ActivityView($("#activityView"), model);
 	var activityViewController = new ActivityViewController(activityView, model);
 
@@ -12,9 +12,6 @@ $(function() {
 
 	var addDayView = new AddDayView($("#addDayView"), model);
 	var addDayViewController = new AddDayViewController(addDayView, model);
-
-	var dayView = new DayView($("#dayView"), model);
-	var dayViewController = new DayViewController(dayView, model);
 
 	var dragDropView = new DragDropView($("#dragDropView"), model);
 	var dragDropViewController = new DragDropViewController(dragDropView, model);
