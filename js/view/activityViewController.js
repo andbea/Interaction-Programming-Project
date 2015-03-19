117125e3a551fd28ce62e19d8c97394242114b5b
andbea <andbea@kth.se>
2015-03-19 15:09:44 +0100

    small change

--- js/view/activityViewController.js
+++ js/view/activityViewController.js
@@ -10,13 +10,17 @@ var ActivityViewController = function(view, model) {
 	view.activitiesContainer.on("dragover", function (ev){
 		ev.preventDefault();
 		view.activitiesContainer.css({"border": "2px solid rgba(101,153,255,0.6)"});
+		model.currentDragOver["dag"] = -1;
+		model.currentDragOver["id"] = 0;
+
 	});
 
 	view.activitiesContainer.on("dragleave", function (ev){
 		ev.preventDefault();
 		view.activitiesContainer.css({"border": "1px solid rgba(211,211,211,1)"});
+		model.currentDragOver["id"] = -1;
 	});
-	
+
 	//makes it posible to drop events
 	view.activitiesContainer.on("drop", function (ev){
 		ev.preventDefault();
